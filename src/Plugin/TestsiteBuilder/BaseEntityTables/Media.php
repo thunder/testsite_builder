43e4288f6d4843112cb081aeb66b7c16cf968ca5
Mladen Todorovic <mladen.todorovic@burda.com>
2019-12-16 11:40:31 +0100

    Added row alter for base entity tables plugin

--- src/Plugin/TestsiteBuilder/BaseEntityTables/Media.php
+++ src/Plugin/TestsiteBuilder/BaseEntityTables/Media.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\testsite_builder\Plugin\TestsiteBuilder\BaseEntityTables;
 
+use Drupal\testsite_builder\ContentCreatorStorage;
+
 /**
  * Media type plugin.
  *
@@ -16,19 +18,18 @@ class Media extends Generic {
   /**
    * {@inheritdoc}
    */
-  public function alterRowTemplate(array &$row_template, array $entity_state_info) {
-    $contentCreatorStorage = $entity_state_info['content_creator_storage'];
-
-    foreach (['media_field_data', 'media_field_revision'] as $table_name) {
-      if (isset($row_template[$table_name])) {
-        $thumbnail = $contentCreatorStorage->getSampleDataType('image');
-        $row_template[$table_name]['thumbnail__target_id'] = $thumbnail['target_id'];
-        $row_template[$table_name]['thumbnail__alt'] = $thumbnail['alt'];
-        $row_template[$table_name]['thumbnail__title'] = $thumbnail['title'];
-        $row_template[$table_name]['thumbnail__width'] = $thumbnail['width'];
-        $row_template[$table_name]['thumbnail__height'] = $thumbnail['height'];
-      }
+  public function alterRow(array &$row, $table_name, ContentCreatorStorage $content_creator_storage) {
+    if ($table_name != 'media_field_data' && $table_name != 'media_field_revision') {
+      return;
     }
+
+    $thumbnail = $content_creator_storage->getModSampleDataType('image', $row['mid']);
+
+    $row['thumbnail__target_id'] = $thumbnail['target_id'];
+    $row['thumbnail__alt'] = $thumbnail['alt'];
+    $row['thumbnail__title'] = $thumbnail['title'];
+    $row['thumbnail__width'] = $thumbnail['width'];
+    $row['thumbnail__height'] = $thumbnail['height'];
   }
 
 }
